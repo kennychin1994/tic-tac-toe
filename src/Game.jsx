f1685c7ceeb45f324cb43a567951aadfe2a7f0cf
kennychin1994 <114026407+kennychin1994@users.noreply.github.com>
2022-09-20 17:31:37 -0400

    Update Game.jsx

--- src/Game.jsx
+++ src/Game.jsx
@@ -19,7 +19,7 @@ class Game extends React.Component {
     const squares = current.squares.slice();
     const locations = this.state.locations.slice(0, this.state.stepNumber + 1);
     const col_row = [Math.floor(i/3+1), i%3+1];
-    if (calculateWinner(squares) || squares[i]) {
+    if (this.calculateWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
@@ -42,10 +42,30 @@ class Game extends React.Component {
     })
   }
   
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+    const winner = this.calculateWinner(current.squares);
     const locations = this.state.locations.slice();
 
     const moves = history.map((step, move) => {
